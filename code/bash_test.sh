f9e96d40673fb2aaf0664c00c01144da39c8214e
sonngocl22 <sonngocl22@gmail.com>
2023-12-01 10:24:18 +0100

    uploading the train_tuned log file for checking

--- code/bash_test.sh
+++ code/bash_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=test
-#SBATCH --output=logs/test%j.log
-#SBATCH --error=logs/test%j.err
+#SBATCH --job-name=tft_train_tuned
+#SBATCH --output=logs/train_tuned%j.log
+#SBATCH --error=logs/train_tuned%j.err
 #SBATCH --mail-user=le004@uni-hildesheim.de
 #SBATCH --partition=NGPU,GPU,STUD
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=gpu-120
 
 echo "Start test"
-srun python /home/le004/master_thesis/git/TSFusionForecast/code/TFT_m4_tune_train.py
+srun python /home/le004/master_thesis/git/TSFusionForecast/code/TFT_m4_tuned_test.py
 echo "End test"
